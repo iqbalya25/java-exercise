fa0a3bc7629fa7def7cf89c16feb26af3bc3f852
iqbalya25 <iqbalya25@gmail.com>
2024-03-25 12:00:31 +0700

    adding exercise 2 function

--- src/main/java/com/adepuu/exercises/session5/Exercise2.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise2.java
@@ -1,5 +1,7 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+
 public class Exercise2 {
     /**
      * Java Array Program For Array Rotation
@@ -9,6 +11,31 @@ public class Exercise2 {
      * So, Final result: 3, 4, 5, 6, 7, 1, 2
      */
     public static void main(String[] args) {
+        int iterator1;
+        int iterator2;
+        int iterator3;
+
+        int[] listNum = {1, 2, 3, 4, 5, 6, 7};
+        int rotationIndexNum = 4;
+        int newdata = listNum.length - 2;
+        ArrayList cutData = new ArrayList<>();
+        ArrayList restData = new ArrayList<>();
+
+        for (iterator1 = 0; iterator1 < rotationIndexNum; iterator1++){
+            System.out.println(listNum[iterator1]);
+            cutData.add(listNum[iterator1]);
+//            System.out.println(cutData);
+        }
+
+        for (iterator2 = rotationIndexNum; iterator2 < listNum.length; iterator2++){
+            System.out.println(listNum[iterator2]);
+            restData.add(listNum[iterator2]);
+//            System.out.println(restData);
+        }
+        for (iterator3 = 0; iterator3 < rotationIndexNum; iterator3++){
+            restData.add(cutData.get(iterator3));
+        }
+        System.out.println(restData);
 
     }
 }
