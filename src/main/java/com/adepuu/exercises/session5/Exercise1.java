544e6fe6343e97d87e0bde0a8ca956e310b07e60
iqbalya25 <iqbalya25@gmail.com>
2024-03-25 11:18:33 +0700

    Add Exercise 1 function

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,20 @@
 package com.adepuu.exercises.session5;
 
 public class Exercise1 {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 35};
+        int maxNumber = findHighNum(arr);
+        System.out.println(maxNumber);
+    }
+    // Iterate through the array to find the maximum number
+    public static int findHighNum (int[] arr){
+        int maxNumber = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > maxNumber) {
+                maxNumber = arr[i];
+            }
+        }
+        return maxNumber;
+    }
 }
