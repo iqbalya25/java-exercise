6b6939ae459494e7c9398594f59c1559ec51e015
iqbalya25 <iqbalya25@gmail.com>
2024-03-26 14:15:30 +0700

    adding session6 exercise 1

--- src/main/java/com/adepuu/exercises/session6/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session6/Exercise1.java
@@ -1,18 +1,43 @@
 package com.adepuu.exercises.session6;
 
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Exercise1 {
-    /**
-     * Write a Java Program to Enter numbers to calculate average and enter 'q' to finish.
-     *
-     * Input: 1, 2, 3, 4, 5, q
-     * Output: 3
-     *
-     * Input: 1, 2, 3, a, b, 4, 5, q
-     * Output: 3
-     *
-     * Explanation: print “Invalid input. Please enter a valid number or 'q' to finish.” if user not inserting the expected number or character.
-     */
+
     public static void main(String[] args) {
+        Scanner inputDataStream = new Scanner(System.in);
+        ArrayList<Integer> listNum = new ArrayList<>();
+        int sum = 0;
+
+        boolean keepLoop = true;
+        while (keepLoop) {
+            System.out.println("input number");
+
+            try {
+                String inputdata = inputDataStream.next();
+                if(inputdata.charAt(0) == 'q') {
+                    for (int num : listNum) {
+                        sum += num;
+                    }
+                    double average = sum / listNum.size();
+                    System.out.println(average);
+                    break;
+                }
+                else {
+                    int numData = Integer.parseInt(inputdata);
+                    listNum.add(numData);
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Input missmatch error");
+
+            } catch (NumberFormatException e) {
+                System.out.println("Number Format error");
+
+            }
+        }
+        inputDataStream.close();
 
     }
 }
